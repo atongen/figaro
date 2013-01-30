cad15e242c533759739620ff96cbb13adfa2b2d4
Andrew Tongen <atongen@nerdery.com>
2013-01-30 12:36:12 -0600

    Use call stack to get root of rack apps

--- lib/figaro.rb
+++ lib/figaro.rb
@@ -1,10 +1,24 @@
 require "shellwords"
 require "pathname"
+require "rbconfig"
 require "figaro/env"
 require "figaro/railtie" if defined?(::Rails)
 require "figaro/tasks"
 
 module Figaro
+
+  class << self
+    attr_accessor :called_from
+
+    def extended(base)
+      base.called_from = begin
+        # Remove the line number from backtraces making sure we don't leave anything behind
+        call_stack = caller.map { |p| p.sub(/:\d+.*/, '') }
+        File.dirname(call_stack.detect { |p| p !~ %r[figaro] })
+      end
+    end
+  end
+
   extend self
 
   def vars(custom_environment = nil)
@@ -32,16 +46,8 @@ module Figaro
     if defined?(::Rails)
       @path = Rails.root.join("config/application.yml")
     else
-      dir = __FILE__
-      begin
-        dir = Pathname.new(File.expand_path(File.join(dir, '..')))
-        if dir.entries.detect { |e| e.to_s == 'config.ru' }
-          @path = dir.join("config/application.yml")
-          break
-        end
-      end until dir.root?
-
-      @path
+      # assumes config.ru in root of rack-based apps
+      @path = find_root_with_flag("config.ru").join("config/application.yml")
     end
   end
 
@@ -57,6 +63,21 @@ module Figaro
 
   def stringify(hash)
     hash.inject({}) { |h, (k, v)| h[k.to_s] = v.to_s; h }
+  end
+
+  def find_root_with_flag(flag, default=nil)
+    root_path = self.called_from
+
+    while root_path && File.directory?(root_path) && !File.exist?("#{root_path}/#{flag}")
+      parent = File.dirname(root_path)
+      root_path = parent != root_path && parent
+    end
+
+    root = File.exist?("#{root_path}/#{flag}") ? root_path : default
+    raise "Could not find root path for #{self}" unless root
+
+    (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/) ?
+      Pathname.new(root).expand_path : Pathname.new(root).realpath
   end
 end
 
