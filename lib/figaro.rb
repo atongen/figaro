b24ccdbed13b02cbee292d13adf5e6e976c5b096
Andrew Tongen <atongen@gmail.com>
2013-01-29 21:20:43 -0600

    Initial addition of rack compatibility

--- lib/figaro.rb
+++ lib/figaro.rb
@@ -1,6 +1,7 @@
 require "shellwords"
+require "pathname"
 require "figaro/env"
-require "figaro/railtie"
+require "figaro/railtie" if defined?(::Rails)
 require "figaro/tasks"
 
 module Figaro
@@ -26,11 +27,26 @@ module Figaro
   end
 
   def path
-    @path ||= Rails.root.join("config/application.yml")
+    return @path if @path
+
+    if defined?(::Rails)
+      @path = Rails.root.join("config/application.yml")
+    else
+      dir = __FILE__
+      begin
+        dir = Pathname.new(File.expand_path(File.join(dir, '..')))
+        if dir.entries.detect { |e| e.to_s == 'config.ru' }
+          @path = dir.join("config/application.yml")
+          break
+        end
+      end until dir.root?
+
+      @path
+    end
   end
 
   def environment
-    Rails.env
+    ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
   end
 
   private
@@ -42,4 +58,8 @@ module Figaro
   def stringify(hash)
     hash.inject({}) { |h, (k, v)| h[k.to_s] = v.to_s; h }
   end
+end
+
+if !defined?(::Rails)
+  ENV.update(Figaro.env)
 end
